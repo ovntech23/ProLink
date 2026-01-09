b11b7811650b9f96c4967833e4fb09c77bb9843b
Ovine Nyalazi <136379264+ovntech23@users.noreply.github.com>
2026-01-09 18:00:00 +0200

    removed apitest section from home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,51 +5,35 @@ import { FeaturesSection } from '../components/home/FeaturesSection';
 import { VisionMissionSection } from '../components/home/VisionMissionSection';
 import { CargoListSection } from '../components/home/CargoListSection';
 import { StatsSection } from '../components/home/StatsSection';
-import { ApiTestSection } from '../components/home/ApiTestSection';
 import { PartnersSection } from '../components/home/PartnersSection';
 import { CtaSection } from '../components/home/CtaSection';
 import { Footer } from '../components/layout/Footer';
 
 export const Home = () => {
   const navigate = useNavigate();
-  
+
   const handleNavigateToLogin = () => {
     navigate('/login');
   };
-  
+
   const handleNavigateToSignup = () => {
     navigate('/signup');
   };
-  
+
   const handleNavigateToTrack = () => {
     navigate('/track');
   };
 
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-      <Navigation 
-        onNavigateToLogin={handleNavigateToLogin}
-        onNavigateToSignup={handleNavigateToSignup}
-      />
-      
-      <HeroSection 
-        onNavigateToSignup={handleNavigateToSignup}
-        onNavigateToTrack={handleNavigateToTrack}
-      />
-      
+      <Navigation onNavigateToLogin={handleNavigateToLogin} onNavigateToSignup={handleNavigateToSignup} />
+      <HeroSection onNavigateToSignup={handleNavigateToSignup} onNavigateToTrack={handleNavigateToTrack} />
       <FeaturesSection />
-      
       <VisionMissionSection />
-      
       <CargoListSection />
-      
-<StatsSection />
-<ApiTestSection />
-      
+      <StatsSection />
       <PartnersSection />
-      
       <CtaSection />
-      
       <Footer />
     </div>
   );
